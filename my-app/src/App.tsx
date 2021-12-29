d7b9d615cb6d47bd6fc2dd791129aea972644698
lengxue <lengxue.xu@tuya.com>
2021-12-29 12:01:40 +0800

    菜单组件

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import useToggle from './hooks/useToggle'
-import Button from './components/button/button'
+import Button from './components/Button/button';
+import Menu from './components/Menu/menu';
+import MenuItem from './components/Menu/menuItem';
 
 function App() {
   const [isToggle, setToggle] = useToggle();
@@ -13,6 +15,13 @@ function App() {
       <Button className='btn' size="small" disabled btnType='link'>disabled</Button>
       <h1>dfdfdf</h1>
       <button className='primary' onClick={setToggle}>{isToggle ? 'Login' : 'Login out'}</button>
+      <div style={{ marginTop: 30 }}>
+        <Menu defaultIndex={0} mode="vertical" onSelect={(index) => { alert(index) }}>
+          <MenuItem index={0}>cool link</MenuItem>
+          <MenuItem index={1}>cool link2</MenuItem>
+          <MenuItem index={2}>cool link3</MenuItem>
+        </Menu>
+      </div>
     </div>
 
   )
